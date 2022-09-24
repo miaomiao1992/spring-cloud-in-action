5df6714782beab3d4baba5cb505abbd561d5cbc1
miaomiao1992 <644284807@qq.com>
2022-09-24 11:00:16 +0800

    2022-09-24-11:00:16-push-miaomiao1992

---
+++ git-push.bat
@@ -0,0 +1,21 @@
+@echo off
+
+set "Time=%time:~0,8%"
+set "t=%Time: =0%"
+
+set "remarks=%date:~,4%-%date:~5,2%-%date:~8,2%-%t%-push-miaomiao1992"
+
+echo start %remarks%
+
+:: add any file you want to push
+git add git-push.bat
+
+git add src
+git add log.txt
+
+git commit -m  %remarks%
+
+git push origin main
+
+echo --End--
+@pause
