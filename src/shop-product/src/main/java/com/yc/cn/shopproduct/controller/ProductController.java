805de39b3fe891f808596a4f4bbb35cca8f0669e
miaomiao1992 <644284807@qq.com>
2022-09-24 20:25:32 +0800

    2022-09-24-20:25:32-push-miaomiao1992

---
+++ src/shop-product/src/main/java/com/yc/cn/shopproduct/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.yc.cn.shopproduct.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/product/{pid}")
+    public String product(@PathVariable("pid") Integer pid) {
+
+        return "222";
+    }
+}
